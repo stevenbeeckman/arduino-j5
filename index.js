02d6386da383563ceb0eccf306c6805c9ef8d4a1
sbeeckman <steven.beeckman+bitbucket@gmail.com>
2014-08-23 16:09:29 +0200

    Added POST request to http://iotbe-njugbe.herokuapp.com

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var five = require("johnny-five")
     , board = new five.Board();
 
-
+var request = require('request');
 
 board.on("ready", function() {
 	// https://github.com/rwaldron/johnny-five/wiki/Sensor
@@ -9,7 +9,7 @@ board.on("ready", function() {
 	
 	var options = {
 	    pin: "A0"
-	    , freq: 500
+	    , freq: 1000
 	    , threshold: 1
 	};
 	photoSensor = new five.Sensor(options);
@@ -18,7 +18,17 @@ board.on("ready", function() {
 	});
 	*/
 	photoSensor.on("data", function(){
-		console.log("Current value of the light sensor is " + this.value + ".");
+		var measurement = this.value;
+		console.log("Current value of the light sensor is " + measurement + ".");
+		request.post(
+			     'http://iotbe-njugbe.herokuapp.com/sensor/0/measurements'
+			     , {value: measurement}
+			     , function(error, response, body){
+				 if(!error && response.statusCode == 200){
+				     console.log(body);
+				 }
+			     }
+);
 	});
 
 
